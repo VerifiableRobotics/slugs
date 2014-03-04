04f3d43822e0332146dda50013e433a2ac64432a
Ruediger Ehlers <Progirep@gmx.net>
2014-03-04 11:32:46 +0100

    Added two benchmark sets (for structured slugs), including support for the SyntComp competition

--- examples/benchmark_generators/factory_assembly_line.py
+++ examples/benchmark_generators/factory_assembly_line.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 #
-# Creates some benchmarks for the evasion of a moving obstacle
+# Creates some benchmarks for an assembly line in a factory.
 
 import math
 import os
@@ -14,11 +14,25 @@ import copy
 
 # ==================================
 # Configurations, consisting of:
-# 1. X Size of the Field
-# 2. Y Size of the Field
-# 3. How many glitches to allow
+# 1. Length of Assembly line
+# 2. Number of tasks to perform on each element
+# 3. Number of steps for which every such number of steps, the belt moves on
+# 4. Error counter: How often it is allowed that not a single task has already been performed on new elements put onto the belt
+#
+# Fixed are:
+# - the number of robot arms is 2
+# - for every incoming new element on the belt, one task has already been performed
 # ==================================
-configurations = [(8,8,0),(8,8,1),(16,16,3),(16,16,4),(24,24,7),(24,24,8),(32,32,11),(32,32,12),(48,48,19),(48,48,20),(64,64,27),(64,64,28),(96,96,43),(96,96,44),(128,128,59),(128,128,60)]
+configurations = [
+                  (5,3,1,0),(5,4,1,0),(5,5,2,0),(5,6,2,0),
+                  (3,3,1,1),(4,3,1,1),
+                                      (5,5,2,1),
+                  (5,3,1,4),          (5,5,2,10),
+                  (5,3,1,5),          (5,5,2,11),
+                  (7,3,1,0),
+                  (7,5,2,0),
+                  (7,5,2,10),(7,5,2,11),
+]
 
 
 
@@ -54,48 +68,66 @@ if len(sys.argv)>1:
 # ==================================
 def makeBenchmarkFile(configuration):
 
-    filenamePrefix = "moving_obstacle_"+str(configuration[0])+"x"+str(configuration[1])+"_"+str(configuration[2])+"glitches"
+    filenamePrefix = "factory_assembly_"+str(configuration[0])+"x"+str(configuration[1])+"_"+str(configuration[2])+"_"+str(configuration[3])+"errors"
     outFile = open(filenamePrefix+".structuredslugs","w")
 
+    maxErrorCount = configuration[3]
+
     text = """[INPUT]
-obsx: 0...%(maxXM1)d
-obsy: 0...%(maxYM1)d
-robx: 0...%(maxX)d
-roby: 0...%(maxY)d
-glitches: 0...%(maxGlitch)d
+movingcounter:0...%(MaxSteps)d
+%(PositionAPs)s
+errorCount:0...%(ErrorCount)d
 
 [OUTPUT]
-movx: 0...2
-movy: 0...2
-obsmove
-
-[SYS_INIT]
-! obsmove
-
-
-[SYS_TRANS]
-(robx < obsx) | (robx >= obsx+2) | (roby < obsy) | (roby >= obsy+2)
-(obsmove' -> ( (obsx=obsx') & (obsy=obsy') ) ) 
-((! obsmove') -> ! ( (obsx=obsx') & (obsy=obsy') ) )
-
-
-[ENV_INIT]
-robx = 0
-roby = 0
-obsx = %(maxXM1)d
-obsy = %(maxYM1)d
-glitches = 0
+arm1:0...%(MaxX)d
+arm2:0...%(MaxX)d
+arm1op:0...%(NofTasksMinusOne)d
+arm2op:0...%(NofTasksMinusOne)d
 
 [ENV_TRANS]
-(obsx' <= obsx+1) & (obsy' <= obsy+1) & (obsx <= obsx'+1) & (obsy <= obsy'+1) & (robx' <= robx+1) & (roby' <= roby+1) & (robx <= robx'+1) & (roby <= roby'+1) 
+movingcounter'=0 -> ((%(SomeNextTaskAlreadyDoneAtTheFirstElement)s) | errorCount' > errorCount)
+errorCount' >= errorCount
 
-(robx=0 & robx'=0 & movx<=1 | robx'+1 <= robx+movx) & (robx=%(maxX)d & robx'=%(maxX)d & movx >= 1 | robx'+1 >= robx+movx) & (roby=0 & roby'=0 & movy <= 1 | roby'+1 <= roby+movy) & (roby=%(maxY)d & roby'=%(maxY)d & movy>=1 | roby'+1 >= roby+movy) 
 
-glitches' >= glitches
-(obsmove | (obsx=obsx')) & (obsmove | (obsy=obsy')) | (glitches' >= glitches+1)
+%(LinesFirstElementsUpdated)s
 
+%(LinesNonFirstElementsUpdated)s
+
+# The moving counter counts module
+(movingcounter<%(MaxSteps)d) -> (movingcounter' = movingcounter+1)
+(movingcounter=%(MaxSteps)d) -> (movingcounter' = 0)
+
+[SYS_TRANS]
+arm1' < arm2'
+arm1' <= arm1+1
+arm1 <= arm1'+1
+arm2' <= arm2+1
+arm2 <= arm2'+1
+
+# Everything must be done one step *before* the items are moved away.
+%(SafetyGuarantees)s
+
+[ENV_INIT]
+movingcounter = 0
+%(PositionAPs)s
+
+[SYS_INIT]
+arm1 = 0
+arm2 = 2
+arm1op = 0
+arm2op = 0
 """ 
-    text = text % {'maxX': configuration[0]-1, 'maxY': configuration[1]-1, 'maxXM1': configuration[0]-2, 'maxYM1': configuration[1]-2,'maxGlitch': configuration[2] }
+    text = text % {
+      'MaxSteps': (configuration[2]-1),
+      'MaxX' : (configuration[0]-1),
+      'NofTasksMinusOne' : configuration[1]-1,
+      'PositionAPs': "\n".join(["p"+str(i)+"_"+str(j) for i in xrange(0,configuration[0]) for j in xrange(0,configuration[1])]),
+      'SomeNextTaskAlreadyDoneAtTheFirstElement' : " | ".join(["p0_"+str(j)+"'" for j in xrange(0,configuration[1])]),
+      'LinesFirstElementsUpdated' : "\n".join(["movingcounter'!=0 -> (p0_"+str(j)+"' <-> p0_"+str(j)+" & (movingcounter'!=0) | arm1=0 & arm1op="+str(j)+" | arm2=0 & arm2op="+str(j)+")" for j in xrange(0,configuration[1])]),
+      'LinesNonFirstElementsUpdated': "\n".join(["p%(i)d_%(j)d' <-> (p%(i)d_%(j)d & (movingcounter'!=0) | arm1=%(i)d & arm1op=%(j)d | arm2=%(i)d & arm2op=%(j)d | (p%(im1)d_%(j)d & movingcounter'=0))" % {'i':i, 'j':j,'im1':i-1 } for  i in xrange(1,configuration[0]) for j in xrange(0,configuration[1])]),
+      'SafetyGuarantees': "\n".join(["!(!p%(i)d_%(j)d & movingcounter'=0)" % {'i': configuration[0]-1, 'j':j} for j in xrange(0,configuration[1])]),
+      'ErrorCount': maxErrorCount
+    }
     
     outFile.write(text)
     outFile.close()
