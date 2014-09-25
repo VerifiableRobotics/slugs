380e3832bc704063607b3e80c4a6480f60bf032a
Ioannis Filippidis <jfilippidis@gmail.com>
2014-09-25 14:15:29 -0700

    fix setup.py

--- tools/StructuredSlugsParser/setup.py
+++ tools/StructuredSlugsParser/setup.py
@@ -10,13 +10,12 @@ long_description = (
 setup(
     name='slugs',
     version='0.1',
-    package_dir={'slugs': 'slugs'},
     license='BSD',
     description='Solve generalized reactivity(1) games.',
     long_description=long_description,
     author='Ruediger Ehlers, Vasumathi Raman, and Cameron Finucane',
     #author_email='',
     url = 'https://github.com/LTLMoP/slugs',
-    install_requires=['networkx'],
     keywords = ['linear temporal logic', 'reactive synthesis', 'games'],
+    packages=['slugs'],
 )
